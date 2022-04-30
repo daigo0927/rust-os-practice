d55ed4dbc035e8115ff994ab2c5df05dd34bdbde
Daigo Hirooka <daigo.hirooka@gmail.com>
2022-04-30 19:28:38 +0900

    add Chapter: VGAテキストモード

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,21 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 // この関数はパニック時に呼ばれる
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
-static HELLO: &[u8] = b"Hellow World!";
-
 #[no_mangle] // この関数の名前修飾をしない
-pub extern "C" fn _start() -> ! {
+pub extern "C" fn _start() {
     // リンカはデフォルトで`_start`と言う名前の関数を探すので、
     // この関数がエントリポイントとなる
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
 
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
     loop {}
 }
